808b1cfbc84cf34c8fb4569da2d67e412731f56a
BastienD <biloute.bd@gmail.com>
2023-11-03 05:45:36 +0100

    JS allégé et fetch Delete fonctionnel !

--- FrontEnd/assets/js/script.js
+++ FrontEnd/assets/js/script.js
@@ -6,6 +6,7 @@ function getWorks() {
     .then(json => {
         worksItems = json;
         generateWorks(worksItems);
+        createAdminGallery(worksItems);
     })
     .catch(error => console.error(error))
 }
@@ -62,7 +63,7 @@ function btnColor(_n) {
 getWorks();
 
 
-// ****************************  Loggin  **************************/
+// ********************************************  Loggin  ********************************************/
 function createElementsAdmin() {
 
     //**********************Editor mode header************
@@ -116,8 +117,43 @@ function createElementsAdmin() {
     document.querySelector(".filter-div").style.display = "none";
 
 }
+    //************************ Gallery Admin **********************
+function createAdminGallery(works) {
+container = document.getElementById("galleryEditContainer");
 
-//************************ Login **********************
+for (i=0; i < works.length; i++) {
+    const work = works[i]
+
+    figure = document.createElement("figure");
+    div = document.createElement("div");
+    div.classList.add("trash-container");
+    div.setAttribute("onclick", `deleteFig(${work.id})`)
+    trash = document.createElement("i");
+    trash.classList.add("fa-solid", "fa-trash-can", "fa-sm");
+    image = document.createElement("img");
+    image.src = work.imageUrl;
+
+
+
+
+
+    container.appendChild(figure);
+    figure.appendChild(div);
+    div.appendChild(trash);
+    figure.appendChild(image);
+
+}
+}
+
+function deleteFig(i) {
+    console.log(i);
+    fetch(`http://localhost:5678/api/works/${i}`, {method: "DELETE", headers: {Authorization: `Bearer ${window.localStorage.getItem("token")}`}});
+
+}
+
+
+
+//***************************************** Login *****************************************
 
 function verifyLog() {
     if (window.localStorage.getItem("token") != null){
@@ -142,210 +178,42 @@ document.addEventListener("DOMContentLoaded", function() {
 
 
 
-
 //***************************************** Modale ***************************************
 
 function openModale() {
     let modaleActive = document.querySelector(".modale-full-container");
     modaleActive.style.display="flex";
+    document.getElementById("returnModaleBtn").style.display="none";
 }
 
 function closeModale() {
+    returnModale();
     let modaleSection = document.querySelector(".modale-full-container");
-    let modaleContent = document.querySelector(".modale-container");
-
-
     modaleSection.style.display="none";
-    modaleContent.remove();
-}
-
-function modaleFirstPage() {
-    fetch('http://localhost:5678/api/works', {method: "GET", headers: {"Content-Type": "application/json"}})
-    .then(response => response.json())
-    .then(json => {
-        worksItems = json;
-        firstModaleGeneration(worksItems)
-    })
-    .catch(error => console.error(error))
-}
-
-function switchSecondToFirst() {
-    closeModale();
-    modaleFirstPage();
-}
-
-function firstModaleGeneration(works) {
-
-    openModale();
-
-    // Modale container
-    let modaleSection = document.getElementById("modaleFullContainer");
-    let mainContainer = document.createElement("div");
-    mainContainer.classList.add("modale-container");
-    modaleSection.appendChild(mainContainer);
-
-
-    // Nav bouton
-    let nav = document.createElement("div");
-    nav.classList.add("nav-modale-btn");
-    mainContainer.appendChild(nav);
-
-    let crossBtn = document.createElement("i");
-    crossBtn.classList.add("fa-solid", "fa-xmark", "fa-xl", "close-modale-btn");
-    crossBtn.setAttribute("onclick", "closeModale()");
-    nav.appendChild(crossBtn);
-
-
-    // Title 
-    let modaleTitle = document.createElement("span");
-    modaleTitle.classList.add("modale-title");
-    modaleTitle.innerHTML="Galerie Photo";
-    mainContainer.appendChild(modaleTitle);
-
-
-    // Gallery Container
-    let container = document.createElement("div");
-    container.classList.add("gallery-editor-container");
-    container.setAttribute("id", "galleryEditContainer");
-    mainContainer.appendChild(container);
-
-
-    // Btn
-    let addPhotoBtn = document.createElement("span");
-    addPhotoBtn.classList.add("btn-add-photo");
-    addPhotoBtn.setAttribute("onclick", "modaleSecondPage()");
-    addPhotoBtn.innerHTML="Ajouter une photo";
-    mainContainer.appendChild(addPhotoBtn);
-
-    for (i=0; i < works.length; i++) {
-        const work = works[i]
-
-        figure = document.createElement("figure");
-        div = document.createElement("div");
-        div.classList.add("trash-container");
-        trash = document.createElement("i");
-        trash.classList.add("fa-solid", "fa-trash-can", "fa-sm");
-        image = document.createElement("img");
-        image.src = work.imageUrl;
-
-
-
-        container.appendChild(figure);
-        figure.appendChild(div);
-        div.appendChild(trash);
-        figure.appendChild(image);
-
-    }
-    
 }
 
 function modaleSecondPage() {
-
-    closeModale();
-    openModale();
-
-    // Modale container
-    let modaleSection = document.getElementById("modaleFullContainer");
-    let mainContainer = document.createElement("div");
-    mainContainer.classList.add("modale-container");
-    modaleSection.appendChild(mainContainer);
-
-
-    // Nav bouton
-    let nav = document.createElement("div");
-    nav.classList.add("nav-modale-btn");
-    mainContainer.appendChild(nav);
-
-    let crossBtn = document.createElement("i");
-    crossBtn.classList.add("fa-solid", "fa-xmark", "fa-xl", "close-modale-btn");
-    crossBtn.setAttribute("onclick", "closeModale()");
-    nav.appendChild(crossBtn);
-
-    let leftBtn = document.createElement("i");
-    leftBtn.classList.add("fa-solid", "fa-arrow-left", "fa-xl", "left-modale-btn");
-    leftBtn.setAttribute("onclick", "closeModale()");
-    leftBtn.setAttribute("onclick", "switchSecondToFirst()")
-    nav.prepend(leftBtn);
-    nav.style="justify-content: space-between";
-
-    // Title 
-    let modaleTitle = document.createElement("span");
-    modaleTitle.classList.add("modale-title");
-    modaleTitle.innerHTML="Galerie Photo";
-    mainContainer.appendChild(modaleTitle);
-
-
-    // Form Container
-    let container = document.createElement("div");
-    container.classList.add("form-modale-container");
-    mainContainer.appendChild(container);
-
-    // Form
-    let submitImageForm = document.createElement("form");
-    submitImageForm.classList.add("submit-image-form");
-    container.appendChild(submitImageForm);
-
-    let imgFileLabel = document.createElement("label");
-    imgFileLabel.classList.add("label-file");
-    imgFileLabel.setAttribute("for", "imgFile");
-    submitImageForm.appendChild(imgFileLabel);
-    let imgFileLabelFa = document.createElement("i");
-    imgFileLabelFa.classList.add("far", "fa-image", "fa-2x");
-    imgFileLabel.appendChild(imgFileLabelFa);
-    let imgFileLabelBtn = document.createElement("span");
-    imgFileLabelBtn.classList.add("add-img-from-btn");
-    imgFileLabelBtn.innerHTML="+ Ajouter une photo";
-    imgFileLabel.appendChild(imgFileLabelBtn);
-    let inputImg = document.createElement("input");
-    inputImg.classList.add("img-file-input");
-    inputImg.setAttribute("type", "file");
-    inputImg.setAttribute("id", "imgFile");
-    inputImg.setAttribute("accept", "image/png, image/jpeg");
-    submitImageForm.appendChild(inputImg);
-
-    let titleLabel = document.createElement("label");
-    titleLabel.classList.add("input-title");
-    titleLabel.setAttribute("for", "title");
-    titleLabel.innerHTML="Titre";
-    submitImageForm.appendChild(titleLabel);
-    let titleInput = document.createElement("input");
-    titleInput.setAttribute("type", "text");
-    titleInput.setAttribute("id", "title");
-    submitImageForm.appendChild(titleInput);
-
-    let categoryTitle = document.createElement("label");
-    categoryTitle.classList.add("input-title");
-    categoryTitle.setAttribute("for", "category");
-    categoryTitle.innerHTML="Catégorie";
-    submitImageForm.appendChild(categoryTitle);
-    let categoryInput = document.createElement("select");
-    categoryInput.classList.add("category-select");
-    categoryInput.setAttribute("id", "category");
-    submitImageForm.appendChild(categoryInput);
-    let optionNull = document.createElement("option");
-    categoryInput.appendChild(optionNull);
-    let optionObject = document.createElement("option");
-    optionObject.setAttribute("value", "1");
-    optionObject.innerHTML="Objets";
-    categoryInput.appendChild(optionObject);
-    let optionAppartements = document.createElement("option");
-    optionAppartements.setAttribute("value", "2");
-    optionAppartements.innerHTML="Appartements";
-    categoryInput.appendChild(optionAppartements);
-    let optionHotel = document.createElement("option");
-    optionHotel.setAttribute("value", "3");
-    optionHotel.innerHTML="Hôtels et Restaurants";
-    categoryInput.appendChild(optionHotel);
-
-    // Btn validation
-    let addPhotoBtn = document.createElement("span");
-    addPhotoBtn.classList.add("btn-validation");
-    addPhotoBtn.innerHTML="Valider";
-    mainContainer.appendChild(addPhotoBtn);
+    document.querySelector(".btn-add-photo").style.display="none";
+    document.getElementById("galleryEditContainer").style.display="none";
+    document.querySelector(".submit-image-form").style.display="flex";
+    document.querySelector(".btn-validation").style.display="flex";
+    let leftBtn = document.getElementById("returnModaleBtn");
+    leftBtn.style.display="flex";
+    let navigationDiv = document.querySelector(".nav-modale-btn");
+    navigationDiv.style="justify-content: space-between";
+    document.querySelector(".modale-title").innerHTML="Ajout Photo";
 
 }
 
+function returnModale() {
+    document.querySelector(".btn-add-photo").style.display="flex";
+    document.getElementById("galleryEditContainer").style.display="flex";
+    document.querySelector(".submit-image-form").style.display="none";
+    document.querySelector(".btn-validation").style.display="none";
+    document.querySelector(".nav-modale-btn").style="justify-content: flex-end";
+    document.getElementById("returnModaleBtn").style.display="none";
+    document.querySelector(".modale-title").innerHTML="Galerie Photo";
 
-
+}
 
 
